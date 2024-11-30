f629ebd7765f5f09c5f1bd9ad01bfc2afb850830
Omoyele.Ewaregbemi <ewaregbemi@gmail.com>
2024-11-30 15:35:30 +0000

    git assignment

---
+++ yele.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+who_am_i() {
+    echo "Hi, i am a cloud student"
+}
+
+fullname() {
+echo "Omoyele Oluwaseyi Ewaregbemi"
+}
+
+bio() {
+echo "male"
+}
+years () {
+echo "0"
+}
+
+dream () {
+echo "jobless"
+}
+goals() {
+echo  "i want to be rich"
+echo  "i want to buy house"
+echo  "i want to buy cars"
+}
+
+goals
